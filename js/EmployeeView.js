f3f65af5df293fbc58b7e4b1f6a2968a23d35170
mlopezbuendia <info@mlopezbuendia.es>
2013-05-13 16:47:22 +0200

    Finished App

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -1,4 +1,43 @@
-var EmployeeView = function (){
+var EmployeeView = function (employee){
+    
+    this.initialize = function(){
+        this.el = $('<div/>');
+    };
+    
+    this.addLocation = function(event){
+        event.preventDefault();
+        console.log("addLocation");
+        navigator.geolocation.getCurrentPosition(
+                function(position){
+                    $('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
+                },
+                function(error){
+                    switch(error.code) {
+                        case error.PERMISSION_DENIED:
+                            alert("User denied the request for Geolocation.");
+                            break;
+                        case error.POSITION_UNAVAILABLE:
+                            alert("Location information is unavailable.");
+                            break;
+                        case error.TIMEOUT:
+                            alert("The request to get user location timed out.");
+                            break;
+                        case error.UNKNOWN_ERROR:
+                            alert("An unknown error occurred.");
+                            break;
+                        }
+                }
+        );
+        return false;
+    };
+    
+    this.render = function(){
+      this.el.on('click', '.add-location-btn', this.addLocation);
+      this.el.html(EmployeeView.template(employee));
+      return this;
+    };
+    
+    this.initialize();
     
 };
 
